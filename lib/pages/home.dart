77aba876d9cc240e46923ec4d7f4134d0fdc1a01
Bobby-Migs <rionjumiguel@gmail.com>
2021-02-15 20:14:24 +0800

    5th committ!!

---
+++ lib/pages/home.dart
@@ -0,0 +1,224 @@
+import 'package:cycle_up/provider/google_sign_in.dart';
+import 'package:cycle_up/widget/sign_up_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:cycle_up/pages/login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+//my own imports
+import 'package:cycle_up/components/horizontal_listview.dart';
+import 'package:cycle_up/components/products.dart';
+import 'package:cycle_up/pages/cart.dart';
+
+//trial
+//import 'package:cycle_up/widget/logged_in_widget.dart';
+import 'package:provider/provider.dart';
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//     body: ChangeNotifierProvider(
+//       create: (context) => GoogleSignInProvider(),
+//       child: StreamBuilder(
+//         stream: FirebaseAuth.instance.authStateChanges(),
+//         builder: (context, snapshot) {
+//           final provider = Provider.of<GoogleSignInProvider>(context);
+//
+//           if (provider.isSigningIn) {
+//             return buildLoading();
+//           } else if (snapshot.hasData) {
+//             return LoggedInWidget();
+//           } else {
+//             return SignUpWidget();
+//           }
+//         },
+//       ),
+//     ),
+//   );
+//
+//   Widget buildLoading() => Stack(
+//     fit: StackFit.expand,
+//     children: [
+//       Center(child: CircularProgressIndicator()),
+//     ],
+//   );
+// }
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser;
+  final googleSignIn = GoogleSignIn();
+  @override
+  Widget build(BuildContext context) {
+    Widget imageCarousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/bannerImg1.jpg'),
+          AssetImage('images/bannerImg2.jpg'),
+          AssetImage('images/bannerImg3.jpg'),
+        ],
+        autoplay: false,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 6.0,
+        dotBgColor: Colors.transparent,
+      ),
+    );
+
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.2,
+        backgroundColor: Colors.red,
+        title: Text('Cycle Up'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> new Cart()));
+              })
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            //  header
+            new UserAccountsDrawerHeader(
+              accountName: Text(
+                'Name: ' + user.displayName,
+              ),
+              accountEmail: Text(
+                'Email: ' + user.email,
+                style: TextStyle(color: Colors.white),
+              ),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  maxRadius: 25,
+                  backgroundImage: NetworkImage(user.photoURL),
+                ),
+              ),
+              decoration: new BoxDecoration(color: Colors.red),
+            ),
+            //  body
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.red),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person, color: Colors.red),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Rentals'),
+                leading: Icon(Icons.directions_bike_outlined, color: Colors.red),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> new Cart()));
+              },
+              child: ListTile(
+                title: Text('My Cart'),
+                leading: Icon(Icons.shopping_cart, color: Colors.red),
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {
+
+                //Navigator.push(context, MaterialPageRoute(builder: (context)=> new SignUpWidget()));
+                final provider =
+                Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.logout();
+              },
+              child: ListTile(
+                title: Text('Logout'),
+                leading: Icon(Icons.logout),
+
+              ),
+            ),
+
+          ],
+        ),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          //START OF IMAGE CAROUSEL
+          imageCarousel,
+          //PADDING WIDGET
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text('Categories'),
+          ), //Padding
+
+          //START OF HORIZONTAL LIST VIEW
+          HorizontalList(),
+          //PADDING WIDGET
+          new Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: new Text('Recent Bikes'),
+          ),
+
+          //GRID VIEW
+          Container(
+            height: 320.0,
+            child: Products(),
+          )
+        ],
+      ),
+    );
+  }
+
+
+void logout() async {
+  await googleSignIn.disconnect();
+  FirebaseAuth.instance.signOut();
+}
+}
---
+++ lib/pages/product_details.dart
@@ -0,0 +1,530 @@
+import 'dart:ui';
+
+import 'package:cycle_up/components/horizontal_listview.dart';
+import 'package:flutter/material.dart';
+import 'package:cycle_up/main.dart';
+import 'package:cycle_up/pages/home.dart';
+import 'package:flutter/services.dart';
+
+class ProductDetails extends StatefulWidget {
+  final prod_detail_name;
+  final prod_detail_new_price;
+  final prod_detail_old_price;
+  final prod_detail_picture;
+  final prod_detail_frameset;
+
+
+  ProductDetails({this.prod_detail_name,
+    this.prod_detail_new_price,
+    this.prod_detail_old_price,
+    this.prod_detail_picture,
+    this.prod_detail_frameset
+  });
+
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+
+class _ProductDetailsState extends State<ProductDetails> {
+
+  String dropdownValueTwo = '';
+  String dropdownValue = '';
+  bool disabledropdown = true;
+  List<DropdownMenuItem<String>> menuitems = List();
+
+  final Day = {
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+  };
+  final Hour = {
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+  };
+
+  void populateday(){
+    for(String key in Day.keys){
+      menuitems.add(DropdownMenuItem<String>(
+        value: Day[key],
+        child: Center(
+          child: Text(
+            Day[key],
+          ),
+        ),
+      ));
+    }
+  }
+  void populateHour(){
+    for(String key in Hour.keys){
+      menuitems.add(DropdownMenuItem<String>(
+        value: Hour[key],
+        child: Center(
+          child: Text(
+            Hour[key],
+          ),
+        ),
+      ));
+    }
+  }
+
+  void valuechanged(_value){
+    if(_value == "Day"){
+      populateday();
+    }else if(_value =="Hour"){
+      populateHour();
+    }
+    setState((){
+      dropdownValue = _value;
+      disabledropdown = false;
+    });
+  }
+
+  void secondvaluechanged(_value){
+    setState((){
+      dropdownValue = _value;
+    });
+  }
+
+  // DATE VARIABLE
+  DateTime selectedDate = DateTime.now();
+  // HOURS VARIABLE
+  final myController = TextEditingController();
+  // QUANTITY DROPDOWN
+
+
+  //<<<<<< OVERRIDE FOR HOURS >>>>>>
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: InkWell(
+            onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> new HomePage()));},
+            child: Text('Cycle Up')),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: GridTile(
+              child: Container(
+                color: Colors.white,
+                child: Image.asset(widget.prod_detail_picture),
+              ),
+              footer: new Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: new Text(
+                    widget.prod_detail_name,
+                    style:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: new Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: new Text(
+                            "${widget.prod_detail_old_price}",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                decoration: TextDecoration.lineThrough),
+                          )),
+                      Expanded(
+                          child: new Text(
+                            "\$${widget.prod_detail_new_price}",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.red),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // ======== the first buttons ===========
+          Row(
+            children: <Widget>[
+
+              //<<<<<<<<<<<<<<< the DATE button >>>>>>>>>>>>
+              Expanded(
+                child: MaterialButton(
+
+                  onPressed: () => _selectDate(context),
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(children: <Widget>[
+                    Expanded(child: new Text("Date")),
+                    Expanded(child: new Icon(Icons.arrow_drop_down)),
+                  ]),
+
+                ),
+              ),
+
+              //<<<<<<<<<<<<<<< the hour button >>>>>>>>>>>>
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(context: context, builder: (context) {
+                      return StatefulBuilder(
+                      builder: (context, setState) {
+                        return AlertDialog(
+                          title: new Text("Rent Type"),
+                          content: Padding(padding: const EdgeInsets.all(16.0),
+                            //child: TextField(
+                            // controller: myController,
+                            // keyboardType: TextInputType.number,
+                            // inputFormatters: <TextInputFormatter>[
+                            //   FilteringTextInputFormatter.digitsOnly
+                            // ],
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.all(5.0),
+
+                                  child: DropdownButton<String>(
+                                    items: [
+                                      DropdownMenuItem<String>(
+                                        value: "Day",
+                                        child: Center(
+                                          child: Text("Day"),
+                                        ),
+                                      ),
+                                      DropdownMenuItem<String>(
+                                        value: "Hour",
+                                        child: Center(
+                                          child: Text("Hour"),
+                                        ),
+                                      ),
+                                    ],
+                                    onChanged: (_value) => {valuechanged(_value),
+                                    setState(() {
+                                    dropdownValue = _value;
+                                    }),
+                                    },
+                                    hint: Text("Day/Hr",
+                                        style: TextStyle(color: Colors.grey)),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: DropdownButton<String>(
+                                    items: menuitems,
+                                    onChanged: disabledropdown ? null : (
+                                        _value) => {secondvaluechanged(_value),
+                                    setState(() {
+                                    dropdownValue = _value;
+                                    }),
+                                    },
+                                    hint: Text("Select day/hr",
+                                        style: TextStyle(color: Colors.grey)),
+                                    disabledHint: Text(
+                                      "First select number",
+                                    ),
+                                  ),
+
+                                ),
+
+                                Text("$dropdownValue"),
+                              ],
+                            ),
+
+                          ),
+                          //),
+
+                          actions: <Widget>[
+                            new MaterialButton(onPressed: () {
+                              Navigator.of(context).pop(context);
+                            },
+                              child: new Text("Ok"),)
+                          ],
+                        );
+
+                      },
+                      );
+                    });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(children: <Widget>[
+                    Expanded(child: new Text("Day/Hr")),
+                    Expanded(child: new Icon(Icons.arrow_drop_down)),
+                  ],),
+                ),
+              ),
+
+              //<<<<<<<<<<<<<<< the QUANTITY button >>>>>>>>>>>>
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: DropdownButton<String>(
+                        items: [
+                          DropdownMenuItem<String>(
+                              value: "1",
+                              child: Center(
+                                child: Text("1"),
+                              ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: "2",
+                            child: Center(
+                              child: Text("2"),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: "3",
+                            child: Center(
+                              child: Text("3"),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: "4",
+                            child: Center(
+                              child: Text("4"),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: "5",
+                            child: Center(
+                              child: Text("5"),
+                            ),
+                          ),
+                        ],
+                        onChanged: (_value) => {
+                          print(_value.toString()),
+                          setState((){
+                            dropdownValueTwo = _value;
+                          }),
+                        },
+                        hint: Text("Qty", style: TextStyle(color: Colors.grey)),
+                      ),
+                    ),
+                    // Text("$dropdownValue"),
+                  ],
+                ),
+
+              ),
+
+            ],
+          ),
+
+          // ======== the second buttons ===========
+          Row(
+            children: <Widget>[
+              //<<<<<<<<<<<<<<< the size button >>>>>>>>>>>
+
+              Expanded(
+                child: MaterialButton(
+                    onPressed: () {},
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    elevation: 0.2,
+                    child: new Text("Rent Now")),
+              ),
+
+              new IconButton(
+                  icon: Icon(Icons.add_shopping_cart),
+                  color: Colors.red,
+                  onPressed: () {}),
+              new IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: () {}),
+            ],
+          ),
+          Divider(),
+          new ListTile(
+            title: new Text("FEATURES"),
+            subtitle: new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "),
+          ),
+          Divider(),
+          new ListTile(
+            title: new Text("SPECIFICATION"),
+          ),
+          new Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+              child: new Text("FRAMESET",style: TextStyle(color: Colors.grey),),)
+            ]
+          ),
+
+          new Row(
+              children: <Widget>[
+                Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                  child: new Text("FORK",style: TextStyle(color: Colors.grey),),),
+                Padding(padding: EdgeInsets.all(5.0),
+                child: new Text(widget.prod_detail_frameset),
+                ),
+              ],
+          ),
+
+          new Row(
+              children: <Widget>[
+                Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                  child: new Text("CRANKS",style: TextStyle(color: Colors.grey),),)
+              ]
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text("Similar Bikes"),
+          ),
+          //SIMILAR PRODUCTS SECTION
+          Container(
+            height: 360.0,
+            child: Similar_products(),
+          ),
+        ],
+      ),
+    );
+  }
+//  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DATE METHOD >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate, // Refer step 1
+      firstDate: DateTime(2021),
+      lastDate: DateTime(2022),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+}
+
+class Similar_products extends StatefulWidget {
+  @override
+  _Similar_productsState createState() => _Similar_productsState();
+}
+
+class _Similar_productsState extends State<Similar_products> {
+  var product_list = [
+    {
+      "name": "SD AM",
+      "picture": "images/products/SD_AM.png",
+      "old_price": 150,
+      "price": 100,
+      "frameset": "Full Cr-Mo Frame ",
+    },
+    {
+      "name": "Cantina",
+      "picture": "images/products/cantina.png",
+      "old_price": 150,
+      "price": 100,
+      "frameset": "Full Cr-Mo Frame ",
+    },
+    {
+      "name": "Cantina",
+      "picture": "images/products/cantina.png",
+      "old_price": 150,
+      "price": 100,
+      "frameset": "Full Cr-Mo Frame ",
+    },
+    {
+      "name": "Cantina",
+      "picture": "images/products/cantina.png",
+      "old_price": 150,
+      "price": 100,
+      "frameset": "Full Cr-Mo Frame ",
+    },
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        itemCount: product_list.length,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return Similar_single_prod(
+            prod_name: product_list[index]['name'],
+            prod_picture: product_list[index]['picture'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_price: product_list[index]['price'],
+            prod_frameset: product_list[index]['frameset'],
+          );
+        });
+  }
+}
+
+class Similar_single_prod extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_price;
+  final prod_old_price;
+  final prod_frameset;
+
+  Similar_single_prod(
+      {this.prod_name,
+        this.prod_picture,
+        this.prod_old_price,
+        this.prod_price,
+        this.prod_frameset
+      });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+          tag: new Text("hero 1"),
+          child: Material(
+            child: InkWell(
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                // here we are passing the values of the product to the product details page
+                  builder: (context) => new ProductDetails(
+                    prod_detail_name: prod_name,
+                    prod_detail_new_price: prod_price,
+                    prod_detail_old_price: prod_old_price,
+                    prod_detail_picture: prod_picture,
+                    prod_detail_frameset: prod_frameset,
+                  ))),
+              child: GridTile(
+                  footer: Container(
+                      color: Colors.white70,
+                      child: new Row(children: <Widget>[
+                        Expanded(
+                          child: Text(prod_name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),),
+                        ),
+                        new Text("\Php${prod_price}", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),)
+                      ],)
+                  ),
+                  child: Image.asset(
+                    prod_picture,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          )),
+    );
+  }
+
+}
+
