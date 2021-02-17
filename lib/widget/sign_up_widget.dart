53d22e4583ed81e22b321de48b9df941134de5ef
Bobby-Migs <rionjumiguel@gmail.com>
2021-02-17 20:33:39 +0800

    6th commit!

--- lib/widget/sign_up_widget.dart
+++ lib/widget/sign_up_widget.dart
@@ -9,22 +9,35 @@ class SignUpWidget extends StatelessWidget {
     children: [
       //CustomPaint(painter: BackgroundPainter()),
       buildSignUp(),
+      new Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/bannerImg3.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          padding: EdgeInsets.only(top: 50.0),
+          child: buildSignUp())
     ],
   );
 
+
   Widget buildSignUp() => Column(
     children: [
+
       Spacer(),
       Align(
         alignment: Alignment.centerLeft,
+
         child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          width: 175,
+
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          width: 200,
           child: Text(
             'Welcome To Cycle Up',
             style: TextStyle(
-              color: Colors.red,
-              fontSize: 30,
+              color: Colors.white,
+              fontSize: 40,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -35,7 +48,7 @@ class SignUpWidget extends StatelessWidget {
       SizedBox(height: 12),
       Text(
         'Login to continue',
-        style: TextStyle(fontSize: 16),
+        style: TextStyle(fontSize: 16, color: Colors.white,),
       ),
       Spacer(),
     ],
